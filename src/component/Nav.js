a90b440b9af0363fe366a6bcb7341a63decc047b
Eddie Bassey <eddie.bassey@nology.io>
2022-06-03 14:26:24 +0100

    my first commit, added nav bar and styles

---
+++ src/component/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Nav = () => {
+	return (
+		<nav>
+			<p className="logo">
+				crypto<span className="light">Dailies</span>
+			</p>
+			<ul>
+				<li>Dashboard</li>
+				<li>About</li>
+			</ul>
+		</nav>
+	);
+};
+
+export default Nav;
